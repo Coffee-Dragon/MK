c3e2d5f9e573e22efe741047914b7a422c545125
Coffee-Dragon <alina.offic@gmail.com>
2021-11-12 00:18:24 +0100

    task2: rewritten function createPlayer for accepting player parameters from the call

--- main.js
+++ main.js
@@ -17,37 +17,39 @@ const player2 = {
         console.log(player2.name + ' Fight!')
     },
 };
+//в функцию добавила четвертый параметр для картинки, а то ощущение, что у игрока раздвоение личности под разными именами ж)
 
-function createPlayer() {
-    const $player1div = document.createElement('div');
-    $player1div.classList.add('player1');
+function createPlayer(playerClass, playerName, playerLife, playerImg) {
+    const $playerdiv = document.createElement('div');
+    $playerdiv.classList.add(playerClass);
 
     const $progressbardiv = document.createElement('div');
     $progressbardiv.classList.add('progressbar');
 
     const $lifediv = document.createElement('div');
     $lifediv.classList.add('life');
-    $lifediv.style.width = '100%';
+    $lifediv.style.width = `${playerLife}%`;
 
     const $namediv = document.createElement('div');
     $namediv.classList.add('name');
-    $namediv.innerText = 'Loki';
+    $namediv.innerText = playerName;
 
     const $characterdiv = document.createElement('div');
     $characterdiv.classList.add('character');
     const $img = document.createElement('img');
-    $img.src = 'http://reactmarathon-api.herokuapp.com/assets/liukang.gif';
+    $img.src = `http://reactmarathon-api.herokuapp.com/assets/${playerImg}.gif`;
     $characterdiv.appendChild($img);
 
     $progressbardiv.appendChild($lifediv);
     $progressbardiv.appendChild($namediv);
 
-    $player1div.appendChild($progressbardiv);
-    $player1div.appendChild($characterdiv);
+    $playerdiv.appendChild($progressbardiv);
+    $playerdiv.appendChild($characterdiv);
 
 
     const $arenasdiv = document.querySelector('.arenas');
-    $arenasdiv.appendChild($player1div);
+    $arenasdiv.appendChild($playerdiv);
 }
 
-createPlayer();
+createPlayer('player1', 'Loki', 90, 'kitana');
+createPlayer('player2', 'Thor', 50, 'liukang');
