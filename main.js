39054b10abcb6addae056882f026d21ab745da82
Coffee-Dragon <alina.offic@gmail.com>
2021-11-11 22:46:09 +0100

    task1: added function for creating playerrepresentation in DOM

--- main.js
+++ main.js
@@ -18,5 +18,36 @@ const player2 = {
     },
 };
 
-player1.attack();
+function createPlayer() {
+    const $player1div = document.createElement('div');
+    $player1div.classList.add('player1');
 
+    const $progressbardiv = document.createElement('div');
+    $progressbardiv.classList.add('progressbar');
+
+    const $lifediv = document.createElement('div');
+    $lifediv.classList.add('life');
+    $lifediv.style.width = '100%';
+
+    const $namediv = document.createElement('div');
+    $namediv.classList.add('name');
+    $namediv.innerText = 'Loki';
+
+    const $characterdiv = document.createElement('div');
+    $characterdiv.classList.add('character');
+    const $img = document.createElement('img');
+    $img.src = 'http://reactmarathon-api.herokuapp.com/assets/liukang.gif';
+    $characterdiv.appendChild($img);
+
+    $progressbardiv.appendChild($lifediv);
+    $progressbardiv.appendChild($namediv);
+
+    $player1div.appendChild($progressbardiv);
+    $player1div.appendChild($characterdiv);
+
+
+    const $arenasdiv = document.querySelector('.arenas');
+    $arenasdiv.appendChild($player1div);
+}
+
+createPlayer();
