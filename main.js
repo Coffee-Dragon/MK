b1afc7d0e3ba7bb364a293ed392c2122474a0f34
Coffee-Dragon <alina.offic@gmail.com>
2021-11-11 21:23:07 +0100

    task0: added player1 and player2 objects

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-alert('Fight...');
+const player1 = {
+    name: '',
+    hp: 0,
+    img: '',
+    weapon: ['sword', 'bow', 'knife', 'morgenshtern', 'fireball'],
+    attack: function() {
+        console.log(player1.name + ' Fight!')
+    },
+};
+
+const player2 = {
+    name: '',
+    hp: 0,
+    img: '',
+    weapon: ['sword', 'bow', 'knife', 'morgenshtern', 'fireball'],
+    attack: function() {
+        console.log(player2.name + ' Fight!')
+    },
+};
+
+player1.attack();
+
